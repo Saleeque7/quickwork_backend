2a9aaf464e0c31199a0c260659108e52b55bfe5d
saleeque <saleeque00cr7@gmail.com>
2024-08-28 18:20:05 +0530

    after adding cookie path

--- Backend/src/libs/utils/Jwt/generateToken.js
+++ Backend/src/libs/utils/Jwt/generateToken.js
@@ -26,7 +26,8 @@ export const generateToken = (res,data,userRole) => {
       httpOnly:true,
       secure:process.env.NODE_ENV !== 'development',
       sameSite:'strict',
-      maxAge:30 * 24 * 60 * 60 * 1000
+      maxAge:30 * 24 * 60 * 60 * 1000,
+      path: '/'
    })
 
   return {
