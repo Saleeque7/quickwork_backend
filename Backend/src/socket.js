9c098cbe3528e0416b29cc788674a236ce4114aa
saleeque <saleeque00cr7@gmail.com>
2024-09-06 03:53:09 +0530

    after adding typing sttaus

--- Backend/src/socket.js
+++ Backend/src/socket.js
@@ -12,17 +12,40 @@ export default function setupSocketHandlers(io) {
       io.emit("get-users", activeUsers);
     });
 
-    socket.on("send-message",(data)=>{
-      const {receiverId} = data
-      const user =activeUsers.find((user)=>user.userId === receiverId)
-      console.log("sending from  soket to :",receiverId);
-      console.log("sending from  soket to ui  data",data);
-      if(user){
-        io.to(user.socketId).emit('receive-message',data)
+    socket.on("send-message", (data) => {
+      const { receiverId } = data
+      const user = activeUsers.find((user) => user.userId === receiverId)
+      console.log("sending from  soket to :", receiverId);
+      console.log("sending from  soket to ui  data", data);
+      if (user) {
+        io.to(user.socketId).emit('receive-message', data)
       }
     })
 
-   
+
+    socket.on('typing', ({ userId }) => {
+      console.log("User typing:", userId);
+
+      // Find all active users except the sender
+      activeUsers.forEach(user => {
+        if (user.userId !== userId) {
+          io.to(user.socketId).emit('typing', { userId });
+        }
+      });
+    });
+
+    socket.on('stop-typing', ({ userId }) => {
+      console.log("User stopped typing:", userId);
+
+ 
+      activeUsers.forEach(user => {
+        if (user.userId !== userId) {
+          io.to(user.socketId).emit('stop-typing', { userId });
+        }
+      });
+    });
+
+
     socket.on("disconnect", () => {
       activeUsers = activeUsers.filter((user) => user.socketId !== socket.id);
       console.log("User Disconnected", activeUsers);
